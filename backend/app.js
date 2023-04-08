786ead98f79f9ff466a84b77cda7c42bb5b6de95
Xlaperr <88415760+Xlaperr@users.noreply.github.com>
2023-04-08 17:05:09 +0700

    backend
    create folder backend (nothing)

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hi Everything");
+});
+
+app.listen(3000, () => {
+  console.log("Start server at port 3000.");
+});
