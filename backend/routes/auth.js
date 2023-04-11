0843c79ed96625aea6538b1785658fc3df57319d
Xlaperr <64070169@it.kmitl.ac.th>
2023-04-11 23:31:03 +0700

    feat: register api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,11 +3,49 @@ const express = require("express"),
 const db = require("../config/db");
 
 router.get("/user", async function (req, res) {
-  db.query(`SELECT * FROM User`, (error, results, fields) => {
+  db.query(`SELECT * FROM User`, (error, results) => {
     if (error) {
       throw error;
     }
     res.json(results);
+  });
+});
+
+router.post("/register", async function (req, res) {
+  const {
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+    gender,
+    birthday,
+    phoneNumber,
+  } = req.body;
+
+  const { address, country, province } = req.body.address;
+
+  let userId;
+
+  const createUser = `INSERT INTO User (firstName, lastName, username, email, password, gender, birthday, phoneNumber) VALUES ("${firstName}", "${lastName}", "${username}", "${email}", "${password}", "${gender}", "${birthday}", "${phoneNumber}")`;
+
+  const createAddress = `INSERT INTO Address (address, country, province, user_id) VALUES ("${address}", "${country}", "${province}", "${userId}")`;
+
+  db.query(createUser, (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      const res = results.json();
+      userId = res[0].user.id;
+    }
+  });
+
+  db.query(createAddress, (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.json(results);
+    }
   });
 });
 
