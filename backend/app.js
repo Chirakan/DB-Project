dc2bd5952189d839e55df72abe77d0a0e25d0cd5
Xlaperr <64070169@it.kmitl.ac.th>
2023-04-12 01:16:23 +0700

    feat: get user by id

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 var authRoute = require("./routes/auth");
+var buyRoute = require("./routes/buy");
 
 app.get("/", (req, res) => {
   res.send("Server is running!!!!!");
@@ -9,6 +10,7 @@ app.get("/", (req, res) => {
 app.use(express.json());
 
 app.use("/auth", authRoute);
+app.use("/buy", buyRoute);
 
 app.listen(3000, () => {
   console.log("Start server at port http://localhost:3000");
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,33 @@
 const express = require("express"),
   router = express.Router();
 const db = require("../config/db");
+const bcrypt = require("bcrypt");
 
-router.get("/allUser", async function (req, res) {
+router.get("/user", async function (req, res) {
   db.query(`SELECT * FROM User`, (error, results) => {
     if (error) {
       throw error;
     }
     res.json(results);
   });
+});
+
+router.get("/user/:id", async function (req, res) {
+  const { id } = req.params;
+
+  db.query(
+    `SELECT * FROM User
+      INNER JOIN Address ON User.user_id = Address.user_id
+    WHERE
+      User.user_id = ?`,
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.json(results[0]);
+    }
+  );
 });
 
 router.post("/register", async function (req, res) {
@@ -23,27 +42,46 @@ router.post("/register", async function (req, res) {
     phoneNumber,
   } = req.body;
 
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
   const { address, country, province } = req.body.address;
 
-  const createUser = `INSERT INTO User (firstName, lastName, username, email, password, gender, birthday, phoneNumber) VALUES ("${firstName}", "${lastName}", "${username}", "${email}", "${password}", "${gender}", "${birthday}", "${phoneNumber}")`;
+  const createUser = `INSERT INTO User (firstName, lastName, username, email, password, gender, birthday, phoneNumber) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-  db.query(createUser, (error, results, fields) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const createAddress = `INSERT INTO Address (address, country, province, user_id) VALUES ("${address}", "${country}", "${province}", ${results.insertId})`;
+  db.query(
+    createUser,
+    [
+      firstName,
+      lastName,
+      username,
+      email,
+      hashedPassword,
+      gender,
+      birthday,
+      phoneNumber,
+    ],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const createAddress = `INSERT INTO Address (address, country, province, user_id) VALUES (?, ?, ?, ?)`;
 
-      const userId = results.insertId;
+        const userId = results.insertId;
 
-      db.query(createAddress, (error, results) => {
-        if (error) {
-          console.log(error);
-        } else {
-          res.send(`Created user ${firstName} with id ${userId}`);
-        }
-      });
+        db.query(
+          createAddress,
+          [address, country, province, results.insertId],
+          (error, results) => {
+            if (error) {
+              console.log(error);
+            } else {
+              res.send(`Created user ${firstName} with id ${userId}`);
+            }
+          }
+        );
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
