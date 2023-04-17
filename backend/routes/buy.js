df8947a25c235c89e21b6305f6861be957c0bc22
Chirakan <64070130@it.kmitl.ac.th>
2023-04-18 01:46:43 +0700

    Update buy.js

--- backend/routes/buy.js
+++ backend/routes/buy.js
@@ -2,4 +2,13 @@ const express = require("express"), //โหลด module express
   router = express.Router();
 const db = require("../config/db");
 
+router.post('/ticket', function (req, res) {
+  const {
+      concert_id, 
+      ticket_zone_id, 
+      seat_name, 
+      purchase_date, 
+  } = req.body
+});
+
 module.exports = router;
