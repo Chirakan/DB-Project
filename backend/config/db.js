9e8baed8b47aae6060ad6782249bb177315f1a06
Xlapper <64070169@it.kmitl.ac.th>
2023-04-19 01:56:00 +0700

    feat: db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,16 @@
-// var mysql = require("mysql2");
-// require("dotenv").config();
+var mysql = require("mysql2");
+require("dotenv").config();
 
-// const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
-// module.exports = db;
-
-const mysql = require('mysql2');
-
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'DB',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+// const db = mysql.createPool({
+//   host: process.env.DATABASE_HOST,
+//   user: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: "db-ticket",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 module.exports = db;
